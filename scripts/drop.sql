7e7f293f5fbcefcd89f26edd515186bf116a826b
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-10 18:12:38 -0300

    Add Contributions to the database

---
+++ scripts/drop.sql
@@ -0,0 +1,8 @@
+-- Drops
+DROP TABLE IF EXISTS parcel_states;
+DROP TABLE IF EXISTS projects;
+DROP TABLE IF EXISTS district_entries;
+
+DROP TABLE IF EXISTS districts;
+DROP TABLE IF EXISTS parcels;
+DROP TABLE IF EXISTS contributions;
