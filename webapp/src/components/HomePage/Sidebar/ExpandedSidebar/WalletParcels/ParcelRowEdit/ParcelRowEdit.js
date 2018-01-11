7413161cbffbc2c73760bef2417ef989450aa1d8
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-11 17:41:11 -0300

    Avoid `null` name/descriptions on inputs

--- webapp/src/components/HomePage/Sidebar/ExpandedSidebar/WalletParcels/ParcelRowEdit/ParcelRowEdit.js
+++ webapp/src/components/HomePage/Sidebar/ExpandedSidebar/WalletParcels/ParcelRowEdit/ParcelRowEdit.js
@@ -58,7 +58,7 @@ class ParcelRowEdit extends React.PureComponent {
                 type="text"
                 name="name-field"
                 id="name-field"
-                value={name}
+                value={name || ''}
                 onChange={this.handleNameChange}
               />
             </div>
@@ -68,7 +68,7 @@ class ParcelRowEdit extends React.PureComponent {
               <textarea
                 name="description"
                 id="description-field"
-                value={description}
+                value={description || ''}
                 onChange={this.handleDescriptionChange}
               />
             </div>
