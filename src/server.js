a893ee97a85362b8fa68fb3fd81f9f7494068682
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-15 14:20:52 -0300

    Group contributions by district

--- src/server.js
+++ src/server.js
@@ -130,7 +130,7 @@ app.get(
 
 export async function getAddressContributions(req) {
   const address = server.extractFromReq(req, 'address')
-  const districts = await Contribution.findByAddress(address)
+  const districts = await Contribution.findGroupedByAddress(address)
 
   return utils.mapOmit(districts, [
     'message',
