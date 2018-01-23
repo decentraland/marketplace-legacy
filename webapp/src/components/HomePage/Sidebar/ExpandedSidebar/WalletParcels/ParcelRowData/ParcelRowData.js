969ed289b6909f9088ac0a724570bf5a3f166b48
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-23 20:08:59 +0100

    Don't blow up on empty prices

--- webapp/src/components/HomePage/Sidebar/ExpandedSidebar/WalletParcels/ParcelRowData/ParcelRowData.js
+++ webapp/src/components/HomePage/Sidebar/ExpandedSidebar/WalletParcels/ParcelRowData/ParcelRowData.js
@@ -3,16 +3,20 @@ import Icon from 'components/Icon'
 import CoordinateLink from '../CoordinateLink'
 
 class ParcelRowData extends React.PureComponent {
+  getPrice() {
+    const { parcel } = this.props
+    return parcel.price ? `${parcel.price} MANA` : '--'
+  }
+
   render() {
     const { parcel, onEdit } = this.props
+
     return (
       <div className="table-row">
         <div className="col col-coord">
           <CoordinateLink parcel={parcel} />
         </div>
-        <div className="col col-price">
-          {parcel.price.toLocaleString()} MANA
-        </div>
+        <div className="col col-price">{this.getPrice()}</div>
         <div className="col col-name">{parcel.name}</div>
         <div className="col col-actions" onClick={onEdit}>
           <Icon name="pencil" />
