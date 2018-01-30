6d956dbaf947f7ba936de600c3eec9c5c5963d84
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-01-30 10:24:03 +0100

    Toast action shortcuts

--- webapp/src/modules/ui/toast/actions.js
+++ webapp/src/modules/ui/toast/actions.js
@@ -1,7 +1,23 @@
 export const OPEN_TOAST = 'Open Toast'
 export const CLOSE_TOAST = 'Close Toast'
 
-export function openToast(message, kind, id, delay) {
+export function openInfoToast(message) {
+  return openToast('info', message)
+}
+
+export function openSuccessToast(message) {
+  return openToast('success', message)
+}
+
+export function openErrorToast(message) {
+  return openToast('error', message)
+}
+
+export function openWarningToast(message) {
+  return openToast('warning', message)
+}
+
+export function openToast({ id, kind, message, delay } = {}) {
   return {
     type: OPEN_TOAST,
     id: id || Date.now().toString(),
