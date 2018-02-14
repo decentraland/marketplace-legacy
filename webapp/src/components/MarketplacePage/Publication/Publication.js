6413bd8ca6289ccf6d69b6a537ee826e84f36dec
Juan Cazala <juancazala@gmail.com>
2018-02-14 12:15:30 -0300

    use locations instead of hardcoded path

--- webapp/src/components/MarketplacePage/Publication/Publication.js
+++ webapp/src/components/MarketplacePage/Publication/Publication.js
@@ -1,11 +1,13 @@
 import React from 'react'
-import { Header, Card, Button, Icon } from 'semantic-ui-react'
 import { Link } from 'react-router-dom'
-import { publicationType } from 'components/types'
 import { txUtils } from 'decentraland-commons'
-import TxStatus from 'components/TxStatus'
 import distanceInWordsToNow from 'date-fns/distance_in_words_to_now'
 import format from 'date-fns/format'
+import { Header, Card, Button, Icon } from 'semantic-ui-react'
+import TxStatus from 'components/TxStatus'
+import { locations } from 'locations'
+import { publicationType } from 'components/types'
+
 import './Publication.css'
 
 export default class Publication extends React.PureComponent {
@@ -21,7 +23,7 @@ export default class Publication extends React.PureComponent {
     return (
       <Card className="Publication">
         <Card.Content>
-          <Link to={`/${publication.x}/${publication.y}`}>
+          <Link to={locations.parcelMapDetail(publication.x, publication.y)}>
             <Icon name="map" />
             {publication.x},{publication.y}
           </Link>
