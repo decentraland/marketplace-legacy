b2da70bb55b8b7fef481eddf409b2ac94b0a5d94
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-02-21 14:29:58 +0100

    Add database migrations

---
+++ migrations/1519217345419_parcels-add-owner-and-data.js
@@ -0,0 +1,14 @@
+import { Parcel } from '../src/Parcel'
+
+const tableName = Parcel.tableName
+
+exports.up = pgm => {
+  pgm.addColumns(tableName, {
+    owner: {
+      type: 'VARCHAR(42)'
+    },
+    data: {
+      type: 'json'
+    }
+  })
+}
