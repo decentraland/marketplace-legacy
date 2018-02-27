2b2be6c3bc5db6714095dcbb7633807f6dcb4e1c
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-02-27 14:26:15 +0100

    feat: Link to activity page on transaction pending

--- webapp/src/components/TxStatus/TxStatusParcel/TxStatusParcel.js
+++ webapp/src/components/TxStatus/TxStatusParcel/TxStatusParcel.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
 
+import { locations } from 'locations'
 import { Message } from 'semantic-ui-react'
 import { transactionType, parcelType } from 'components/types'
 import ParcelName from 'components/ParcelName'
@@ -23,7 +25,9 @@ export default class TxStatusParcel extends React.PureComponent {
     return transactions.length ? (
       <Message warning className="TxStatusParcel">
         <ParcelName parcel={parcel} />&nbsp;&nbsp;still has&nbsp;
-        {transactions.length} pending transactions
+        {transactions.length} pending transactions.<br />
+        To see your transaction history in more detail, visit your&nbsp;
+        <Link to={locations.activity}>activity page</Link>.
       </Message>
     ) : null
   }
