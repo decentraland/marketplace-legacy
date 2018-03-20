086a9667098b5a29606415d20a56a8191769ccd4
Juan Cazala <cazala@mail.sfsu.edu>
2018-03-20 17:30:24 -0300

    fix: show 0 balance (#138)
    * fix: show 0 balance
    
    * fix: replaced typeof with >=

--- webapp/src/components/Mana/Mana.js
+++ webapp/src/components/Mana/Mana.js
@@ -21,7 +21,7 @@ export default class Mana extends React.PureComponent {
   getLocalizedAmount() {
     const { amount } = this.props
 
-    if (amount) {
+    if (amount >= 0) {
       return amount.toLocaleString()
     }
   }
