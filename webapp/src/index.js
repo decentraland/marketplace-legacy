89b9a992e3762f24d3e40423457cfa684b00098b
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-03-21 21:53:54 +0100

    fix: trim state only once

--- webapp/src/index.js
+++ webapp/src/index.js
@@ -16,6 +16,29 @@ import './index.css'
 
 env.load()
 
+// TEMPORAL CODE
+// Trims down the stored local storage to avoid hitting the browser limit
+if (!window.localStorage.getItem('__clean__')) {
+  let state = window.localStorage.getItem('decentraland-marketplace')
+  state = JSON.parse(state)
+
+  const { transaction } = state
+
+  if (transaction && transaction.data) {
+    transaction.data = transaction.data.map(tx => ({
+      hash: tx.hash,
+      payload: tx.payload,
+      timestamp: tx.timestamp,
+      from: tx.from,
+      actionType: tx.actionType,
+      status: tx.status
+    }))
+  }
+  window.localStorage.setItem('decentraland-marketplace', JSON.stringify(state))
+  window.localStorage.removeItem('decentraland-marketplace-translations')
+  window.localStorage.setItem('__clean__', 1)
+}
+
 ReactDOM.render(
   <Provider store={store}>
     <ConnectedRouter history={history}>
