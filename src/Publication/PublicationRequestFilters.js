e076fe60a6f92971f808acc80a195cf5bf77a31e
Juan Cazala <juan@decentraland.org>
2018-03-21 19:08:20 -0300

    chore: removed DEFAULT_VALUES

--- src/Publication/PublicationRequestFilters.js
+++ src/Publication/PublicationRequestFilters.js
@@ -1,12 +1,5 @@
 import { server } from 'decentraland-commons'
 
-const DEFAULT_VALUES = Object.freeze({
-  sort_by: 'created_at',
-  sort_order: 'ASC',
-  limit: 20,
-  offset: 0
-})
-
 const ALLOWED_VALUES = Object.freeze({
   price: ['ASC'],
   created_at: ['DESC'],
@@ -14,12 +7,8 @@ const ALLOWED_VALUES = Object.freeze({
 })
 
 export class PublicationRequestFilters {
-  static getDefaultValues() {
-    return DEFAULT_VALUES
-  }
-
   static getAllowedValues() {
-    return DEFAULT_VALUES
+    return ALLOWED_VALUES
   }
 
   constructor(req) {
@@ -32,7 +21,7 @@ export class PublicationRequestFilters {
     let limit = server.extractFromReq(this.req, 'limit')
     let offset = server.extractFromReq(this.req, 'offset')
 
-    by = by in ALLOWED_VALUES ? by : DEFAULT_VALUES['sort_by']
+    by = by in ALLOWED_VALUES ? by : 'created_at'
     order = ALLOWED_VALUES[by].includes(order.toUpperCase())
       ? order
       : ALLOWED_VALUES[by][0]
