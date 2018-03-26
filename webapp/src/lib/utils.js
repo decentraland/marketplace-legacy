b8d7786b211b070cc81e96386249bfc552612875
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-03-26 13:45:47 +0200

    feat: only connect to the Ledger wallet if we're on a desktop browser

--- webapp/src/lib/utils.js
+++ webapp/src/lib/utils.js
@@ -53,6 +53,17 @@ export const cancelAnimationFrame =
   window.msCancelAnimationFrame ||
   (id => clearTimeout(id))
 
+export function isMobile() {
+  // WARN: Super naive mobile device check.
+  // we're using it on low-stake checks, where failing to detect some browsers is not a big deal.
+  // If you need more specificity you may want to change this implementation.
+  const navigator = window.navigator
+
+  return (
+    /Mobi/i.test(navigator.userAgent) || /Android/i.test(navigator.userAgent)
+  )
+}
+
 export function formatMana(amount, unit = 'MANA') {
   const amountNumber = parseFloat(amount, 10)
   return `${amountNumber.toLocaleString()} ${unit}`.trim()
--- webapp/src/modules/wallet/utils.js
+++ webapp/src/modules/wallet/utils.js
@@ -1,14 +1,17 @@
-import { env, eth, utils, contracts } from 'decentraland-commons'
+import { env, eth, utils, contracts, wallets } from 'decentraland-commons'
+import { isMobile } from 'lib/utils'
 
 export async function connectEthereumWallet(options = {}, retries = 0) {
   try {
     const { MANAToken, LANDRegistry, Marketplace } = contracts
+    const { LedgerWallet, NodeWallet } = wallets
     const providerUrl = env.get('REACT_APP_PROVIDER_URL')
 
     let connected = await eth.connect({
       ...options,
       providerUrl,
-      contracts: [MANAToken, LANDRegistry, Marketplace]
+      contracts: [MANAToken, LANDRegistry, Marketplace],
+      wallets: isMobile() ? [NodeWallet] : [LedgerWallet, NodeWallet]
     })
     if (!connected) throw new Error('Could not connect to Ethereum')
   } catch (error) {
