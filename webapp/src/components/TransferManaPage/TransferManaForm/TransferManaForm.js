22f5e7fcf6ce474a0b5839b3aabaec33dbad81ad
Ariel Barmat <abarmat@decentraland.org>
2018-04-05 16:20:04 -0400

    chore: lint fix

--- webapp/src/components/TransferManaPage/TransferManaForm/TransferManaForm.js
+++ webapp/src/components/TransferManaPage/TransferManaForm/TransferManaForm.js
@@ -2,7 +2,7 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { eth } from 'decentraland-commons'
 
-import { Button, Form, Input, Message } from 'semantic-ui-react'
+import { Button, Form, Input } from 'semantic-ui-react'
 import TxStatus from 'components/TxStatus'
 import { preventDefault } from 'lib/utils'
 import { t } from 'modules/translation/utils'
