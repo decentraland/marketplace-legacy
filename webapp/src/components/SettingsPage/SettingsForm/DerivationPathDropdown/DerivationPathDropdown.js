c36823d9b9f0d3e100fb120381976eede2d1608f
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-04-11 23:10:55 +0200

    feat: derivation path feedback (#191)
    * feat: add a valid check and text for derivation paths
    
    * feat: new derivation path error message
    
    * feat: error color

--- webapp/src/components/SettingsPage/SettingsForm/DerivationPathDropdown/DerivationPathDropdown.js
+++ webapp/src/components/SettingsPage/SettingsForm/DerivationPathDropdown/DerivationPathDropdown.js
@@ -2,11 +2,12 @@ import React from 'react'
 import { Dropdown } from 'semantic-ui-react'
 import { t } from 'modules/translation/utils'
 
+import './DerivationPathDropdown.css'
+
 const derivationOptions = [
   {
     text: "m/44'/60'/0'/0",
-    description:
-      'Jaxx, Metamask, Exodus, imToken, Ledger & TREZOR (ETH) & Digital Bitbox'
+    description: 'Metamask, imToken, Ledger, TREZOR (ETH), etc'
   },
   { text: "m/44'/60'/160720'/0'", description: 'Ledger (ETC)' },
   { text: "m/44'/61'/0'/0", description: 'TREZOR (ETC)' }
@@ -14,26 +15,40 @@ const derivationOptions = [
 
 const DEFAULT_VALUE = derivationOptions[0].value
 const PATH_PREFIX = 'm/'
-const SUPPORTED_PATHS = ["m/44'/60'", "m/44'/61'"]
+const SUPPORTED_STARTING_PATHS = ["m/44'/60'", "m/44'/61'"]
 
 export default class DerivationPathDropdown extends React.PureComponent {
   static propsTypes = Dropdown.propTypes
 
   static defaultProps = {
-    defaultValue: DEFAULT_VALUE
+    value: DEFAULT_VALUE
   }
 
-  handleOnChange = (e, data) => {
-    const derivationPath = data.value
+  constructor(props) {
+    super(props)
 
-    if (this.isValid(derivationPath)) {
-      this.props.onChange(this.removePrefix(derivationPath))
+    this.state = {
+      isValid: true
     }
   }
 
-  getDefaultDerivationPath() {
-    const defaultValue = this.props.defaultValue || DEFAULT_VALUE
-    return this.addPrefix(defaultValue)
+  handleOnChange = (e, data) => {
+    const isValid = this.isValid(data.value)
+
+    if (isValid) {
+      this.props.onChange(this.removePrefix(data.value))
+    }
+
+    this.setState({ isValid })
+  }
+
+  handleOnClick = () => {
+    this.setState({ isValid: true })
+  }
+
+  getDerivationPath() {
+    const derivationPath = this.props.value || DEFAULT_VALUE
+    return this.addPrefix(derivationPath)
   }
 
   addPrefix(path) {
@@ -45,30 +60,26 @@ export default class DerivationPathDropdown extends React.PureComponent {
   }
 
   isValid(derivationPath) {
-    return SUPPORTED_PATHS.some(path => derivationPath.startsWith(path))
+    const validStart = SUPPORTED_STARTING_PATHS.some(path =>
+      derivationPath.startsWith(path)
+    )
+    const validLength = derivationPath.split('/').length === 5
+
+    return validStart && validLength
   }
 
   render() {
-    const defaultValue = this.getDefaultDerivationPath()
+    const { isValid } = this.state
+    const value = this.getDerivationPath()
 
     return (
-      <React.Fragment>
-        <label>
-          Derivation path&nbsp;
-          <strong
-            data-balloon-pos="up"
-            data-balloon-length="xlarge"
-            data-balloon={t('derivation_path.supported', {
-              paths: SUPPORTED_PATHS.join(', ')
-            })}
-          >
-            ?
-          </strong>
-        </label>
+      <div className="DerivationPathDropdown">
+        <label>Derivation path</label>
         <Dropdown
           {...this.props}
           onChange={this.handleOnChange}
-          defaultValue={defaultValue}
+          onClick={this.handleOnClick}
+          value={value}
           options={derivationOptions}
           allowAdditions
           search
@@ -76,7 +87,10 @@ export default class DerivationPathDropdown extends React.PureComponent {
           selection
           required
         />
-      </React.Fragment>
+        <div className="invalid-derivation-path">
+          {!isValid && t('derivation_path.invalid')}
+        </div>
+      </div>
     )
   }
 }
