e71efec100ff69727879b152dbac6cddaf5cf688
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-04-13 19:03:17 +0200

    chore: remove unused script

--- scripts/updateExpiresAtToInt.sql
+++
@@ -1,4 +0,0 @@
-ALTER TABLE publications ADD expires_at_ts BIGINT;
-UPDATE publications SET expires_at_ts = EXTRACT(epoch from expires_at) * 1000;
-ALTER TABLE publications DROP expires_at;
-ALTER TABLE publications RENAME expires_at_ts TO expires_at;
