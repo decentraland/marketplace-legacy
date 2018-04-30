37bf53b7008c8bc75ea4200cb451b8c798c28dc6
Ariel Barmat <abarmat@decentraland.org>
2018-04-30 12:35:47 -0400

    chore: remove ETH to MANA rate

--- webapp/src/components/BuyManaPage/BuyManaForm/BuyManaForm.js
+++ webapp/src/components/BuyManaPage/BuyManaForm/BuyManaForm.js
@@ -173,10 +173,6 @@ export default class BuyManaForm extends React.PureComponent {
             </Form.Field>
             <br />
             <span className="info">
-              {t('buy_mana.rate')}: 1 MANA ={' '}
-              {formatMana(this.state.rate, 'ETH')}
-            </span>
-            <span className="info">
               {t('buy_mana.rate')}: {formatMana(1 / (this.state.rate || 1))} = 1
               ETH
             </span>
