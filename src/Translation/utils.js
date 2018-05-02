54ced7ad0a966968b49ae09efa1ace7e70b6c6a6
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-05-03 00:31:20 +0200

    chore: cleanup package.json. add analyze

--- src/Translation/utils.js
+++
@@ -1,8 +0,0 @@
-import flat from 'flat'
-
-export function parseTranslations(fileContents) {
-  // The translation lib ( https://github.com/yahoo/react-intl ) doesn't support nested values
-  // So instead we flatten the structure to look like `{ 'nested.prop': 'value' }`
-  const translations = JSON.parse(fileContents)
-  return flat(translations)
-}
