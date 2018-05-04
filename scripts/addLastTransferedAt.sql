6a5ee1af085722ee4dd076c82fd6ece4d967afb5
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-05-04 11:35:06 +0200

    fix: always use tranferred

--- scripts/addLastTransferedAt.sql
+++ scripts/addLastTransferedAt.sql
@@ -1,4 +1,4 @@
-ALTER TABLE parcels ADD last_transferred_at BIGINT;
-CREATE INDEX blockchain_events_name_index ON blockchain_events USING BTREE(name);
-CREATE INDEX blockchain_events_args_asset_id_index ON blockchain_events USING BTREE((args->>'assetId'));
+ALTER TABLE parcels ADD IF NOT EXIST last_transferred_at BIGINT;
+CREATE INDEX IF NOT EXISTS blockchain_events_name_index ON blockchain_events USING BTREE(name);
+CREATE INDEX IF NOT EXISTS blockchain_events_args_asset_id_index ON blockchain_events USING BTREE((args->>'assetId'));
 UPDATE parcels P SET last_transferred_at = (SELECT BT.timestamp FROM blockchain_events B, block_timestamps BT WHERE B.block_number = BT.block_number AND B.name = 'Transfer' AND B.args->>'assetId' = P.asset_id ORDER BY BT.block_number DESC LIMIT 1);
