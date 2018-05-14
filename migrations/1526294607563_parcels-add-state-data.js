7628f6b3183ff1687e9d0bd2e671b5a3d4060753
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-05-14 18:30:06 +0200

    feat: first state structure

---
+++ migrations/1526294607563_parcels-add-state-data.js
@@ -0,0 +1,10 @@
+import { Parcel } from '../src/Parcel'
+
+exports.up = pgm => {
+  const tableName = Parcel.tableName
+
+  pgm.addColumns(tableName, {
+    in_state: { type: 'BOOLEAN', default: false, allowNull: false },
+    state_id: { type: 'TEXT' }
+  })
+}
