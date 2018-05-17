9dd8af7b232eecb5368c9cd124293acb661b55b2
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-05-17 14:16:11 +0200

    fix: state -> estate

--- migrations/1526294607563_parcels-add-state-data.js
+++
@@ -1,9 +0,0 @@
-import { Parcel } from '../src/Parcel'
-
-exports.up = pgm => {
-  const tableName = Parcel.tableName
-
-  pgm.addColumns(tableName, {
-    in_state: { type: 'BOOLEAN', default: false, allowNull: false }
-  })
-}
