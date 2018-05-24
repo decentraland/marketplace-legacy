5d851787986d8fcf262e3446615c81772bbe6195
Federico Miras <miras.federico@gmail.com>
2018-05-24 08:35:18 -0300

    chore: add radix parameter to estate modal parse int

--- webapp/src/components/CreateEstatePage/EditEstate/EstateModal/EstateModal.js
+++ webapp/src/components/CreateEstatePage/EditEstate/EstateModal/EstateModal.js
@@ -28,8 +28,8 @@ export default class EstateModal extends React.PureComponent {
     const { parcels } = this.props
     const xs = [...new Set(parcels.map(coords => coords.x).sort())]
     const ys = [...new Set(parcels.map(coords => coords.y).sort())]
-    const x = xs[parseInt(xs.length / 2)]
-    const y = ys[parseInt(ys.length / 2)]
+    const x = xs[parseInt(xs.length / 2, 10)]
+    const y = ys[parseInt(ys.length / 2, 10)]
     const center = { x, y }
     const zoom = 1 / (xs.length + ys.length) * 7.5
     return { center, zoom }
