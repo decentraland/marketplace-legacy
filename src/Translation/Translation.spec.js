80c6808ea8f592dee85de9c41c713ffe4f4674b6
Ignacio <nachomazzara@gmail.com>
2018-06-08 13:04:50 -0300

    feat: remove unused async

--- src/Translation/Translation.spec.js
+++ src/Translation/Translation.spec.js
@@ -6,23 +6,25 @@ const DEFAULT_LOCALE = Translation.DEFAULT_LOCALE
 const translation = new Translation()
 let mainTranslations, availableLocales, mainKeys
 
-describe('Translation', async function() {
+describe('Translation', () => {
   beforeEach(async () => {
     mainTranslations = await translation.fetch(DEFAULT_LOCALE)
     availableLocales = await translation.getAvailableLocales()
     mainKeys = Object.keys(mainTranslations)
   })
   describe('locales', () =>
-    it('should check if locales has the same keys', () =>
+    it('should check if default locale is en', () => {
+      expect(Translation.DEFAULT_LOCALE).to.be.equal('en')
       context('should check if locales has the same keys', () => {
         for (const locale of availableLocales) {
           if (locale !== DEFAULT_LOCALE) {
-            it(`should have the same keys as the default locale. ${locale} = ${DEFAULT_LOCALE}`, async function() {
+            it(`should have the same keys as the default locale. ${locale} = ${DEFAULT_LOCALE}`, async () => {
               const translations = await translation.fetch(locale)
               const keys = Object.keys(translations)
               expect(keys).to.deep.equal(mainKeys)
             })
           }
         }
-      })))
+      })
+    }))
 })
