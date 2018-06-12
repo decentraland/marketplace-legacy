a157b28203d7251a7586447d4248b30e0abf9275
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-06-12 21:42:05 +0200

    feat: Sanity self heal (#249)
    * feat: add a self-heal option to the sanity-check
    
    * feat: run the monitor on self-heal

--- scripts/monitor/program.js
+++ scripts/monitor/program.js
@@ -9,49 +9,47 @@ import { loadEnv } from '../../scripts/utils'
 
 const log = new Log('main')
 
-loadEnv('../../src/.env')
+export async function main(
+  getNewMonitor = (...args) => new MonitorCli(...args)
+) {
+  log.debug('Connecting to database')
+  await db.connect()
 
-Promise.resolve()
-  .then(() => {
-    log.debug('Connecting to database')
-    return db.connect()
+  log.debug('Connecting to Ethereum node')
+  await eth.connect({
+    contracts: [
+      new contracts.LANDRegistry(env.get('LAND_REGISTRY_CONTRACT_ADDRESS')),
+      new contracts.Marketplace(env.get('MARKETPLACE_CONTRACT_ADDRESS')),
+      new contracts.MortgageCreator(
+        env.get('MORTGAGE_CREATOR_CONTRACT_ADDRESS')
+      ),
+      new contracts.RCNEngine(env.get('RCN_ENGINE_CONTRACT_ADDRESS')),
+      new contracts.MortgageManager(
+        env.get('MORTGAGE_MANAGER_CONTRACT_ADDRESS')
+      )
+    ],
+    provider: env.get('RPC_URL')
   })
-  .then(() => {
-    log.debug('Connecting to Ethereum node')
-    return eth.connect({
-      contracts: [
-        new contracts.LANDRegistry(env.get('LAND_REGISTRY_CONTRACT_ADDRESS')),
-        new contracts.Marketplace(env.get('MARKETPLACE_CONTRACT_ADDRESS')),
-        new contracts.MortgageCreator(
-          env.get('MORTGAGE_CREATOR_CONTRACT_ADDRESS')
-        ),
-        new contracts.RCNEngine(env.get('RCN_ENGINE_CONTRACT_ADDRESS')),
-        new contracts.MortgageManager(
-          env.get('MORTGAGE_MANAGER_CONTRACT_ADDRESS')
-        )
-      ],
-      provider: env.get('RPC_URL')
-    })
-  })
-  .then(() => {
-    log.debug('Starting CLI')
 
-    return new MonitorCli(
-      handlers,
-      {
-        Marketplace: [
-          'AuctionCreated',
-          'AuctionSuccessful',
-          'AuctionCancelled'
-        ],
-        LANDRegistry: ['Update', 'Transfer'],
-        MortgageCreator: ['NewMortgage'],
-        MortgageManager: ['CanceledMortgage']
-      },
-      env.get('PROCESS_EVENTS_DELAY', 2 * 60 * 1000) // 2 minutes
-    ).run()
-  })
-  .catch(error => {
+  log.debug('Starting CLI')
+  const monitor = getNewMonitor(
+    handlers,
+    {
+      Marketplace: ['AuctionCreated', 'AuctionSuccessful', 'AuctionCancelled'],
+      LANDRegistry: ['Update', 'Transfer'],
+      MortgageCreator: ['NewMortgage'],
+      MortgageManager: ['CanceledMortgage']
+    },
+    env.get('PROCESS_EVENTS_DELAY', 2 * 60 * 1000) // 2 minutes
+  )
+  await monitor.run()
+}
+
+if (require.main === module) {
+  loadEnv('../../src/.env')
+
+  main().catch(error => {
     log.error(error)
     process.exit()
   })
+}
