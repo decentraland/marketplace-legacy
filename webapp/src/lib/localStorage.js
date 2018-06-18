d4b24014fa96a3f2495c43db16dfceb2ec76ddf5
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-06-18 12:04:19 +0200

    fix: don't blow up on blocked cookies

--- webapp/src/lib/localStorage.js
+++ webapp/src/lib/localStorage.js
@@ -1,5 +1,13 @@
-export const localStorage = window.localStorage || {
-  getItem: () => {},
-  setItem: () => {},
-  removeItem: () => {}
+function getLocalStorage() {
+  try {
+    const localStorage = window.localStorage
+    const val = 'val'
+    localStorage.setItem(val, val)
+    localStorage.removeItem(val)
+    return localStorage
+  } catch (e) {
+    return { getItem: () => {}, setItem: () => {}, removeItem: () => {} }
+  }
 }
+
+export const localStorage = getLocalStorage()
