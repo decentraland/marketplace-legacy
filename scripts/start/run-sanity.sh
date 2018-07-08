21c2075d5d36672a133855423516cc817e63b386
Ariel Barmat <abarmat@decentraland.org>
2018-07-07 20:30:10 -0400

    chore: variable names

--- scripts/start/run-sanity.sh
+++ scripts/start/run-sanity.sh
@@ -1,19 +1,18 @@
 #!/bin/bash
 
-PROCESS_PATH='/home/ubuntu/marketplace'
-PROCESS_NAME='sanityCheck.js'
+PROC_PATH='/home/ubuntu/marketplace'
+PROC_NAME='sanityCheck.js'
 
 log () {
-    echo "(Process:${PROCESS_NAME}) $1"
+    echo "(Process:${PROC_NAME}) $1"
 }
 
 run () {
-    cd $PROCESS_PATH
-    npm run sanity-check -- --self-heal
+    cd $PROC_PATH && npm run sanity-check -- --self-heal
 }
 
 main () {
-    if ! pgrep -f ${PROCESS_NAME} > /dev/null
+    if ! pgrep -f ${PROC_NAME} > /dev/null
     then
         log 'Starting'
         run
