6b92e0cb2f2d0240f53c69cceedd7050e04be77b
Ariel Barmat <abarmat@decentraland.org>
2018-08-06 19:32:57 -0300

    chore: add build env script along with some changes to ansible

---
+++ scripts/build-env.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+ADDRESS_FILE="addresses.json"
+NETWORK=$1
+
+function nodeenv_from_network() {
+  if [ $1 == "mainnet" ]
+  then
+    echo "production"
+  else
+    echo "development"
+  fi
+}
+
+function get_config_item() {
+  echo `jq .${NETWORK}.$1 ${ADDRESS_FILE}`
+}
+
+echo "NODE_ENV=\"$(nodeenv_from_network $1)\""
+echo "WEBAPP_PATH=\"../webapp/build\""
+echo "CONNECTION_STRING=$2"
+echo "RPC_URL=\"http://nodes.decentraland.org:8545\""
+echo "PROCESS_EVENTS_DELAY=30000"
+echo "LAND_REGISTRY_CONTRACT_ADDRESS=$(get_config_item 'LANDRegistry')"
+echo "MARKETPLACE_CONTRACT_ADDRESS=$(get_config_item 'Marketplace')"
+echo "MORTGAGE_CREATOR_CONTRACT_ADDRESS=$(get_config_item 'MortgageHelper')"
+echo "MORTGAGE_MANAGER_CONTRACT_ADDRESS=$(get_config_item 'MortgageManager')"
+echo "RCN_ENGINE_CONTRACT_ADDRESS=$(get_config_item 'RCNEngine')"
