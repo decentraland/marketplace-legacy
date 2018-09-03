3e656a836d87a5962eb2a45eceac3fcf3171f8e0
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-09-03 18:04:47 -0300

    fix: add missing translations

--- webapp/src/components/TxStatus/TxStatusText/TxStatusText.js
+++ webapp/src/components/TxStatus/TxStatusText/TxStatusText.js
@@ -27,7 +27,11 @@ export default class TxStatusText extends React.PureComponent {
           : t('transaction_status.text.failed')}
         &nbsp;
         {t_html('transaction_status.see_activity', {
-          activity_link: <Link to={locations.activity}>activity page</Link>
+          activity_link: (
+            <Link to={locations.activity}>
+              {t('transaction_status.activity_page')}
+            </Link>
+          )
         })}
       </div>
     ) : null
