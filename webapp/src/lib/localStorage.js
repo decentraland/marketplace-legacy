ccdd6a41b30cc618f0017065b96c461027910943
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-09-26 21:54:43 +0200

    feat: localstorage migration

--- webapp/src/lib/localStorage.js
+++ webapp/src/lib/localStorage.js
@@ -1,12 +1,47 @@
+import {
+  ALLOW_TOKEN_SUCCESS,
+  APPROVE_TOKEN_SUCCESS
+} from 'modules/authorization/actions'
+
 export const migrations = {
   2: data => {
     const transaction = data.transaction
-    if (transaction && transaction.data) {
+    if (transaction) {
       transaction.data = transaction.data.map(tx => {
         return {
           ...tx,
           receipt: null // Remove previous receipt. Will save them on demand
         }
+      })
+    }
+    return { ...data, transaction }
+  },
+  3: data => {
+    const APPROVE_MANA_SUCCESS = '[Success] Approve MANA'
+    const AUTHORIZE_LAND_SUCCESS = '[Success] Authorize LAND'
+    const transaction = data.transaction
+
+    if (transaction) {
+      transaction.data = transaction.data.map(tx => {
+        if (tx.actionType === APPROVE_MANA_SUCCESS) {
+          tx.payload = {
+            amount: tx.payload.mana,
+            address: tx.from,
+            contractName: 'Marketplace',
+            tokenContractName: 'MANAToken'
+          }
+          tx.actionType = ALLOW_TOKEN_SUCCESS
+        } else if (tx.actionType === AUTHORIZE_LAND_SUCCESS) {
+          tx.payload = {
+            isApproved: tx.payload.isAuthorized,
+            address: tx.from,
+            contractName: 'Marketplace',
+            tokenContractName: 'LANDRegistry'
+          }
+          tx.actionType = APPROVE_TOKEN_SUCCESS
+        }
+
+        return tx
       })
     }
     return { ...data, transaction }
