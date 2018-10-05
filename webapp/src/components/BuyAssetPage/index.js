5fbd9ada0fac5785405852ea62c1f9bdaf7c721b
Juan Cazala <juan@decentraland.org>
2018-10-05 16:15:46 -0300

    fix: wrong import

--- webapp/src/components/BuyAssetPage/index.js
+++ webapp/src/components/BuyAssetPage/index.js
@@ -1,3 +1,3 @@
-export { BuyWarningMessage } from './BuyWarningMessage'
+export { default as BuyWarningMessage } from './BuyWarningMessage'
 export { Loading } from './Loading'
 export { NotConnected } from './NotConnected'
