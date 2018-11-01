0dd7d21c58516c1d78ebb5d9ddac23e771462272
Juan Cazala <juancazala@gmail.com>
2018-11-01 20:08:32 -0300

    feat: added support for EIP-1102 (#634)

--- webapp/src/components/SignInPage/SignInPage.js
+++ webapp/src/components/SignInPage/SignInPage.js
@@ -20,7 +20,7 @@ export default class SignInPage extends React.PureComponent {
 
   componentWillReceiveProps(nextProps) {
     if (nextProps.isConnected) {
-      this.props.onNavigate(locations.settings())
+      this.props.onNavigate(locations.parcelMapDetail(0, 0))
     }
   }
 
