758b716f45137b251e80b50867db025f6d4c1d6d
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-11-02 00:45:34 +0100

    feat: translate terms modal. simplify css

--- webapp/src/components/Modal/TermsModal/TermsModal.js
+++ webapp/src/components/Modal/TermsModal/TermsModal.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Button } from 'semantic-ui-react'
 
-import { locations } from 'locations'
 import DecentralandLogo from 'components/DecentralandLogo'
-import { getLocalStorage } from '@dapps/lib/localStorage'
+import { t, T } from '@dapps/modules/translation/utils'
+import { agreeToTerms } from 'modules/terms/utils'
 
 import BaseModal from '../BaseModal'
 
@@ -14,74 +14,72 @@ export default class TermsModal extends React.PureComponent {
     ...BaseModal.propTypes
   }
 
-  constructor(props) {
-    super(props)
-    this.state = {
-      acceptedTerms: false
-    }
+  handleOnAgree = () => {
+    agreeToTerms()
+    this.props.onClose()
   }
 
-  handleOnClose = () => {
-    if (this.state.acceptedTerms) {
-      this.props.onClose()
-      const localStorage = getLocalStorage()
-      localStorage.setItem('seenTermsModal', new Date().getTime())
-    }
+  renderTermsOfServiceLink() {
+    return (
+      <a
+        href="https://decentraland.org/terms"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {t('terms_modal.terms_of_service')}
+      </a>
+    )
   }
 
-  onAgree = () => {
-    this.setState({ acceptedTerms: true }, this.handleOnClose)
+  renderPrivacyPolicyLink() {
+    return (
+      <a
+        href="https://decentraland.org/privacy"
+        rel="noopener noreferrer"
+        target="_blank"
+      >
+        {t('terms_modal.privacy_policy')}
+      </a>
+    )
   }
 
   render() {
-    const { pathname } = this.props.location
-    if (pathname === locations.root()) {
-      return null
-    }
     return (
       <BaseModal
         className="TermsModal modal-lg"
+        isCloseable={false}
         {...this.props}
-        onClose={this.handleOnClose}
       >
-        <div className="banner">
-          <h2>
-            <DecentralandLogo />
-          </h2>
-        </div>
+        <DecentralandLogo />
 
         <div className="modal-body">
           <div className="text">
-            <h3>Welcome to Decentraland&apos;s LAND Marketplace</h3>
+            <h2>{t('terms_modal.title')}</h2>
+            <br />
             <p>
-              This dApp interacts with a <strong>decentralized exchange</strong>{' '}
-              that runs on the Ethereum blockchain.
+              <T id="terms_modal.explanation" />
             </p>
             <p>
-              By choosing &quot;I agree&quot; below, you agree to our{' '}
-              <a
-                href="https://decentraland.org/terms"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Terms of Service
-              </a>.
+              <T
+                id="terms_modal.by_agreeing"
+                values={{
+                  terms_of_service_link: this.renderTermsOfServiceLink()
+                }}
+              />
             </p>
             <p>
-              You also agree to our{' '}
-              <a
-                href="https://decentraland.org/privacy"
-                rel="noopener noreferrer"
-                target="_blank"
-              >
-                Privacy Policy
-              </a>, which describes how we process your information.
+              <T
+                id="terms_modal.also_agree"
+                values={{ privacy_policy_link: this.renderPrivacyPolicyLink() }}
+              />
             </p>
+            <br />
+            <br />
           </div>
 
-          <div className="get-started">
-            <Button primary={true} onClick={this.onAgree}>
-              I AGREE
+          <div className="agree-to-terms">
+            <Button primary={true} onClick={this.handleOnAgree}>
+              {t('terms_modal.i_agree').toUpperCase()}
             </Button>
           </div>
         </div>
