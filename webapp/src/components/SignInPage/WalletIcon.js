2943fd14e466431f8edc071821aa881c1b885ed1
Juan Cazala <juan@decentraland.org>
2018-11-26 15:16:27 -0300

    style: fix sign in page colors

--- webapp/src/components/SignInPage/WalletIcon.js
+++ webapp/src/components/SignInPage/WalletIcon.js
@@ -8,29 +8,6 @@ const WalletIcon = () => (
     version="1.1"
     xmlns="http://www.w3.org/2000/svg"
   >
-    <desc>Created with Sketch.</desc>
-    <defs>
-      <linearGradient
-        x1="50%"
-        y1="100%"
-        x2="50%"
-        y2="-2.48949813e-15%"
-        id="linearGradient-112"
-      >
-        <stop stopColor="#1F2333" offset="0%" />
-        <stop stopColor="#0D0F17" offset="100%" />
-      </linearGradient>
-      <linearGradient
-        x1="50%"
-        y1="100%"
-        x2="50%"
-        y2="3.061617e-15%"
-        id="linearGradient-222"
-      >
-        <stop stopColor="#00D7EB" offset="0%" />
-        <stop stopColor="#007B87" offset="100%" />
-      </linearGradient>
-    </defs>
     <g
       id="Marketplace12"
       stroke="none"
@@ -44,17 +21,11 @@ const WalletIcon = () => (
       >
         <g id="Group-5" transform="translate(360.000000, 184.000000)">
           <g id="Group-4" transform="translate(270.000000, 192.000000)">
-            <circle
-              id="Oval-2"
-              fill="url(#linearGradient-112)"
-              cx="90"
-              cy="90"
-              r="90"
-            />
+            <circle id="Oval-2" fill="#272329" cx="90" cy="90" r="90" />
             <path
               d="M125,87.125 L120,87.125 L120,70 L68.5,70 C67.1215,70 66,68.8785 66,67.5 C66,66.1215 67.1215,65 68.5,65 L115,65 L115,60 L68.5,60 C64.3645,60 61,63.3645 61,67.5 L61,111.5 C61,115.6355 64.3645,119 68.5,119 L120,119 L120,102.125 L125,102.125 L125,87.125 Z M115,114 L68.5,114 C67.1215,114 66,112.8785 66,111.5 L66,74.57 C66.7825,74.8475 67.623625,75 68.5,75 L115,75 L115,87.125 L109.25,87.125 C105.1145,87.125 101.75,90.4895 101.75,94.625 C101.75,98.7605 105.1145,102.125 109.25,102.125 L115,102.125 L115,114 Z M120,97.125 L109.25,97.125 C107.8715,97.125 106.75,96.0035 106.75,94.625 C106.75,93.2465 107.8715,92.125 109.25,92.125 L120,92.125 L120,97.125 Z"
               id="Shape"
-              fill="url(#linearGradient-222)"
+              fill="#ffffff"
               fillRule="nonzero"
             />
           </g>
