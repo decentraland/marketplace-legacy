47bda5398b441bf204f53af6cc575fafb6b9a45c
Nicolás Santángelo <santangelonicolas@gmail.com>
2018-11-28 10:03:26 -0300

    Update webapp/src/modules/auction/utils.js
    Co-Authored-By: nachomazzara <nachomazzara@gmail.com>

--- webapp/src/modules/auction/utils.js
+++ webapp/src/modules/auction/utils.js
@@ -57,5 +57,5 @@ export async function isAuctionActive() {
 }
 
 export function getAuctionStartDate() {
-  return 1544400000000 // 10 of December 2018 00:00:00 UTC 0
+  return 1544400000000 // 10th of December 2018 00:00:00 UTC
 }
