544b29c31c49c0797ce6d7194c341e4e655f0aa9
Juan Cazala <juan@decentraland.org>
2018-12-05 18:06:25 -0300

    feat: added banner for started and finished auction

--- webapp/src/components/AuctionCountdown/AuctionCountdown.js
+++ webapp/src/components/AuctionCountdown/AuctionCountdown.js
@@ -2,18 +2,68 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { t } from '@dapps/modules/translation/utils'
 
-import { getAuctionStartDate } from 'modules/auction/utils'
+import { getAuctionStartDate, hasAuctionFinished } from 'modules/auction/utils'
 import Countdown from 'components/Countdown'
 
 import './AuctionCountdown.css'
 
 export default class AuctionCountdown extends React.PureComponent {
   static defaultProps = {
-    isBanner: false
+    isBanner: false,
+    isConnected: false
   }
 
   static propTypes = {
-    isBanner: PropTypes.bool
+    isBanner: PropTypes.bool,
+    isConnected: PropTypes.bool
+  }
+
+  constructor(props) {
+    super(props)
+    this.interval = null
+    this.state = {
+      hasFinished: false
+    }
+  }
+
+  async componentDidMount() {
+    this.stopInterval()
+    if (!this.hasStarted()) {
+      this.interval = setInterval(() => this.checkStartDate(), 1000)
+    }
+    this.checkEndDate()
+  }
+
+  componentWillUnmount() {
+    this.stopInterval()
+  }
+
+  componentWillReceiveProps() {
+    this.checkEndDate()
+  }
+
+  hasStarted() {
+    return Date.now() > getAuctionStartDate()
+  }
+
+  checkStartDate() {
+    if (this.hasStarted()) {
+      window.location.reload()
+      this.stopInterval()
+    }
+  }
+
+  async checkEndDate() {
+    if (!this.props.isConnected) return
+    const hasFinished = await hasAuctionFinished()
+    this.setState({ hasFinished })
+  }
+
+  stopInterval() {
+    if (this.interval) {
+      clearInterval(this.interval)
+      this.interval = null
+    }
   }
 
   getClassName = () => {
@@ -22,14 +72,26 @@ export default class AuctionCountdown extends React.PureComponent {
   }
 
   render() {
+    const { hasFinished } = this.state
+    const hasStarted = this.hasStarted()
+    let description
+    if (hasFinished) {
+      description = t('auction.description_after_finish')
+    } else if (hasStarted) {
+      description = t('auction.description_after_start')
+    } else {
+      description = t('auction.description_before_start')
+    }
     return (
       <div className={`AuctionCountdown ${this.getClassName()}`}>
         <div>
           <h1 className="title">{t('auction.title')}</h1>
-          <div className="description">{t('auction.description')}</div>
+          <div className="description">{description}</div>
         </div>
         <div className="countdown-wrapper">
-          <Countdown date={getAuctionStartDate()} />
+          {hasStarted || hasFinished ? null : (
+            <Countdown date={getAuctionStartDate()} />
+          )}
         </div>
         <div className="actions">{this.props.children}</div>
       </div>
