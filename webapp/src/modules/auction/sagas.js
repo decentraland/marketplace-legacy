a89a5cac976eb4a6ece4c5b4fab5c057c4e223e1
Ignacio Mazzara <nachomazzara@gmail.com>
2018-12-07 18:26:43 -0300

    fix: mana burned and auction duration (#760)

--- webapp/src/modules/auction/sagas.js
+++ webapp/src/modules/auction/sagas.js
@@ -71,7 +71,8 @@ function* handleAuctionParamsRequest(action) {
       gasPriceLimit,
       landsLimitPerBid,
       totalLandsBidded: totalLandsBidded,
-      totalManaBurned: totalManaBurned.toNumber(),
+      totalManaBurned:
+        totalManaBurned.toNumber() > 0 ? eth.utils.fromWei(totalManaBurned) : 0,
       endTime: endTime.toNumber()
     }
 
