bc8497099fd5f0caa740ac479486b9540b9299fe
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-01-03 18:44:26 -0300

    fix: REACT_APP prefix

--- webapp/src/modules/tile/utils.js
+++ webapp/src/modules/tile/utils.js
@@ -1,7 +1,9 @@
 import { env } from 'decentraland-commons'
 
+const DEFAULT_WAIT_TIME = 10000 // 10 seconds
 const waitTimeBetweenNewTilesRequest =
-  parseInt(env.get('WAIT_TIME_BETWEEN_NEW_TILES_REQUEST'), 10) || 10000 // 10 seconds
+  parseInt(env.get('REACT_APP_WAIT_TIME_BETWEEN_NEW_TILES_REQUEST'), 10) ||
+  DEFAULT_WAIT_TIME
 
 let lastTimestamp = Date.now()
 
