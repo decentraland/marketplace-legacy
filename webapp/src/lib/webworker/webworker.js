95debd18763667f1c8df3af4a1a3c33738a7e53d
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-01-14 18:33:41 -0300

    fix: missing normalized parcel (#812)
    * fix: missing normalized parcel
    
    * fix: add normalize parcel to WW

--- webapp/src/lib/webworker/webworker.js
+++ webapp/src/lib/webworker/webworker.js
@@ -1,14 +1,15 @@
 import { WebWorkerFactory } from './WebWorkerFactory'
 import { WebWorkerOnMessage } from './WebWorkerOnMessage'
 import { getAssetPublications } from 'shared/asset'
-import { toParcelObject } from 'shared/parcel'
+import { toParcelObject, normalizeParcel } from 'shared/parcel'
 import { toEstateObject, normalizeEstate } from 'shared/estate'
 
 const WebWorkerDependencies = {
   toParcelObject,
   getAssetPublications,
   toEstateObject,
-  normalizeEstate
+  normalizeEstate,
+  normalizeParcel
 }
 
 export const webworker = WebWorkerFactory.create(
