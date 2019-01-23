da05bee1eebeaf8a6e3667e3b60da07bc88285b5
Ariel Barmat <abarmat@decentraland.org>
2019-01-23 16:29:06 -0300

    chore: remove unused line

--- scripts/start/backup-dump.sh
+++ scripts/start/backup-dump.sh
@@ -15,5 +15,4 @@ BUCKET_TARGET='s3://market-prod.decentraland.org/dumps/'
 pg_dump -h ${DB_HOST} -d ${DB_NAME} -U ${DB_USER} | gzip -f > ${DUMP_FILE_PATH}
 aws s3 cp ${DUMP_FILE_PATH} ${BUCKET_TARGET} --acl public-read
 aws s3 cp ${DUMP_FILE_PATH} ${BUCKET_TARGET}${DUMP_FILE_DATE} --acl public-read
-/bin/date '+%a' | awk '{print tolower($0)}'
 /bin/rm -f ${DUMP_FILE_PATH}
