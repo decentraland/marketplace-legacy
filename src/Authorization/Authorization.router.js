d8cce63453fdd912f7c3f02a4ac1eca5f204853b
Ignacio Mazzara <nacho@decentraland.org>
2019-03-08 17:51:18 -0300

    fix: bad lintin

--- src/Authorization/Authorization.router.js
+++ src/Authorization/Authorization.router.js
@@ -80,21 +80,20 @@ export class AuthorizationRouter {
 
     let authorizations
     if (parcel.estate_id) {
-      const estate = await Estate.findOne({ id : parcel.estate_id})
-      
+      const estate = await Estate.findOne({ id: parcel.estate_id })
+
       authorizations = await this.getAuthorizations(
         estate,
         ASSET_TYPES.estate,
         address
-        )
+      )
     } else {
       authorizations = await this.getAuthorizations(
         parcel,
         ASSET_TYPES.parcel,
         address
       )
-
-    }    
+    }
     return { id: parcel.id, address, ...authorizations }
   }
 
