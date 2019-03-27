6e7deca5014e41a4acd46eb9ce24118f2bab2f94
Ignacio Mazzara <nacho@decentraland.org>
2019-03-27 18:34:02 -0300

    feat: change migration var

--- migrations/1553104304201_blockchain-events-add-indexes.js
+++ migrations/1553104304201_blockchain-events-add-indexes.js
@@ -1,19 +1,15 @@
 import { BlockchainEvent } from '../src/BlockchainEvent'
 
 const tableName = BlockchainEvent.tableName
-const args = 'args->>'
 
 exports.up = pgm => {
-  pgm.createIndex(tableName, `(${args}'assetId')`, {
-    method: 'BTREE'
-  })
-  pgm.createIndex(tableName, `(${args}'landId')`, { method: 'BTREE' })
-  pgm.createIndex(tableName, `(${args}'tokenId')`, { method: 'BTREE' })
-  pgm.createIndex(tableName, `(${args}'_landId')`, { method: 'BTREE' })
-  pgm.createIndex(tableName, `(${args}'_tokenId')`, { method: 'BTREE' })
-  pgm.createIndex(tableName, `(${args}'_estateId')`, { method: 'BTREE' })
-  pgm.createIndex(tableName, `(${args}'_assetId')`, {
-    method: 'BTREE'
-  })
+  /* eslint-disable */
+  pgm.createIndex(tableName, "(args->>'assetId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'landId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'tokenId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'_landId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'_tokenId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'_estateId')", { method: 'BTREE' })
+  pgm.createIndex(tableName, "(args->>'_assetId')", { method: 'BTREE' })
   pgm.createIndex(tableName, 'name')
 }
