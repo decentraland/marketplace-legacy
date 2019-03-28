807a91dae036694fa2cb20de065f05118b9eefd4
Ignacio Mazzara <nacho@decentraland.org>
2019-03-28 09:29:07 -0300

    disable prettier

--- src/Asset/Estate/Estate.queries.js
+++ src/Asset/Estate/Estate.queries.js
@@ -13,19 +13,11 @@ export const EstateQueries = Object.freeze({
     OR ${raw(tableRef)}.asset_type != ${ASSET_TYPES.estate})`,
   areEstateEvents: estateId => {
     const address = env.get('ESTATE_REGISTRY_CONTRACT_ADDRESS')
-
+    // prettier-ignore
     return SQL`${BlockchainEventQueries.byArgs('_estateId', estateId)}
     OR ${BlockchainEventQueries.byArgs('assetId', estateId)}
-    OR ((${BlockchainEventQueries.byArgs(
-      '_tokenId',
-      estateId
-    )} OR ${BlockchainEventQueries.byArgs(
-      '_assetId',
-      estateId
-    )}) AND address = ${address})
-    OR (${BlockchainEventQueries.byArgs(
-      '_tokenId',
-      estateId
-    )} AND ${BlockchainEventQueries.byArgs('_tokenAddress', address)})`
+    OR ((${BlockchainEventQueries.byArgs('_tokenId', estateId)} OR ${BlockchainEventQueries.byArgs('_assetId', estateId)}) 
+      AND address = ${address})
+    OR (${BlockchainEventQueries.byArgs('_tokenId', estateId)} AND ${BlockchainEventQueries.byArgs('_tokenAddress', address)})`
   }
 })
