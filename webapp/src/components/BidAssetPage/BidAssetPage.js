a3b4e972d30037a9a83a44d16ef14a82c912c303
Ignacio Mazzara <nacho@decentraland.org>
2019-03-28 08:57:06 -0300

    feat: show connect if user is logged out

--- webapp/src/components/BidAssetPage/BidAssetPage.js
+++ webapp/src/components/BidAssetPage/BidAssetPage.js
@@ -1,18 +1,21 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Loader } from 'semantic-ui-react'
+import { Loader, Container } from 'semantic-ui-react'
+import { t } from '@dapps/modules/translation/utils'
 
 import { ASSET_TYPES } from 'shared/asset'
 import { bidType } from 'components/types'
 import BidParcelPage from './BidParcelPage'
 import BidEstatePage from './BidEstatePage'
+import SignInNotice from 'components/SignInNotice'
 
 export default class BidAssetPage extends React.PureComponent {
   static propTypes = {
     id: PropTypes.string.isRequired,
     bid: bidType,
     assetType: PropTypes.string.isRequired,
-    isLoading: PropTypes.bool.isRequired
+    isLoading: PropTypes.bool.isRequired,
+    isConnected: PropTypes.bool.isRequired
   }
 
   isAllowed = () =>
@@ -20,12 +23,24 @@ export default class BidAssetPage extends React.PureComponent {
     this.props.authorization.allowances.ERC721Bid.MANAToken > 0
 
   render() {
-    const { assetType, isLoading } = this.props
+    const { assetType, isLoading, isConnected } = this.props
 
     if (isLoading) {
       return (
         <div>
           <Loader active size="massive" />
+        </div>
+      )
+    }
+
+    if (!isConnected) {
+      return (
+        <div>
+          <Container text textAlign="center" className="NotConnected">
+            <SignInNotice
+              title={t('asset_bid.bid_asset', { asset_type: assetType })}
+            />
+          </Container>
         </div>
       )
     }
