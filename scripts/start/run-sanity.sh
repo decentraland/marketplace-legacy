f46fdf99f1dde8111b445531bf66d1b3adbc39de
Ariel Barmat <abarmat@decentraland.org>
2019-03-31 10:58:38 -0300

    chore: update run sanity script

--- scripts/start/run-sanity.sh
+++ scripts/start/run-sanity.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-PROC_PATH='/home/ubuntu/marketplace'
-PROC_NAME='sanityCheck.js'
+PROC_PATH='/home/ubuntu/app'
+PROC_NAME='self-heal'
 
 log () {
     echo "(Process:${PROC_NAME}) $1"
 }
 
 run () {
-    cd $PROC_PATH && npm run sanity-check -- --self-heal
+    cd $PROC_PATH && npm run sanity-check -- --from-block=4900000 --websocket --self-heal
 }
 
 main () {
+    export BATCH_SIZE=100
+    export SKIP_TILES_CACHE_UPDATE=1
+
     if ! pgrep -f ${PROC_NAME} > /dev/null
     then
         log 'Starting'
