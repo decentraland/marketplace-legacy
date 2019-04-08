f40abbf600d845051df3f207e3dfe63259c03a95
Ignacio Mazzara <nacho@decentraland.org>
2019-04-08 11:29:26 -0300

    refactor: change sanity doctors order

--- sanity/doctors/index.js
+++ sanity/doctors/index.js
@@ -2,8 +2,10 @@ import { ParcelDoctor } from './ParcelDoctor'
 import { PublicationDoctor } from './PublicationDoctor'
 import { EstateDoctor } from './EstateDoctor'
 
+// Publications should be treated first in order to avoid inconsistencies for orders
+// where they're not cancelled when changing the owner of the asset
 export const doctors = {
+  Publication: PublicationDoctor,
   Estate: EstateDoctor,
-  Parcel: ParcelDoctor,
-  Publication: PublicationDoctor
+  Parcel: ParcelDoctor
 }
