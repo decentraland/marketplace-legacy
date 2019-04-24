4846a7ff7e702b047d194bf04239041d82e5e425
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-04-24 16:11:10 +0200

    fix: don't add timestamps to approvals

--- src/Approval/Approval.model.js
+++ src/Approval/Approval.model.js
@@ -5,4 +5,5 @@ import { SQL } from '../database'
 export class Approval extends Model {
   static tableName = 'approvals'
   static columnNames = ['type', 'token_address', 'owner', 'operator']
+  static withTimestamps = false
 }
