bbd958451e8b0f0e2d5433e9c1dee3760f21b145
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-04-25 17:37:38 +0200

    chore: remove unused file and code

--- src/Approval/Approval.model.js
+++ src/Approval/Approval.model.js
@@ -1,7 +1,5 @@
 import { Model } from 'decentraland-server'
 
-import { SQL } from '../database'
-
 export class Approval extends Model {
   static tableName = 'approvals'
   static columnNames = ['type', 'token_address', 'owner', 'operator']
