d97bb80017b323d2f3694f27cbbde778e3c0a6bb
Nicolás Santángelo <santangelonicolas@gmail.com>
2019-07-17 13:56:48 +0200

    fix: fix listing queries. Warn potential injection

--- src/Listing/Listing.queries.js
+++ src/Listing/Listing.queries.js
@@ -1,5 +1,5 @@
 import { Listing } from './Listing'
-import { SQL } from '../database'
+import { SQL, raw } from '../database'
 
 export const ListingQueries = Object.freeze({
   isInactive: () => SQL`expires_at < EXTRACT(epoch from now()) * 1000`,
@@ -10,9 +10,14 @@ export const ListingQueries = Object.freeze({
     const selectSQL = Listing.getListableAssets()
       .map(({ tableName }) => `row_to_json(${tableName}.*) as ${tableName}`)
       .join(', ')
-    return SQL(selectSQL)
+
+    return raw(selectSQL)
   },
 
+  /**
+   * WARN: `joinTableName` is used as a raw param on the query
+   * DO NOT use it for user submitted content
+   */
   joinListableAssets: joinTableName => {
     const joinSQL = Listing.getListableAssets()
       .map(
@@ -20,6 +25,7 @@ export const ListingQueries = Object.freeze({
           `LEFT JOIN ${tableName} as ${tableName} ON ${tableName}.id = ${joinTableName}.asset_id`
       )
       .join('\n')
-    return SQL(joinSQL)
+
+    return raw(joinSQL)
   }
 })
